3f03b4f777cf847522d584ff4dd6228b8ef8aced
Krishan <krishan.pandey_cs19@gla.ac.in>
2020-10-27 22:41:47 +0530

    final commit

--- src/labTask/Main.java
+++ src/labTask/Main.java
@@ -1,4 +1,16 @@
 package labTask;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 public class Main {
+    public static void main(String[] args) {
+        int currentDate = new GregorianCalendar().get(Calendar.DATE);
+        String currentMonth = new GregorianCalendar().getDisplayName(Calendar.MONTH, Calendar.LONG,
+                Locale.ENGLISH);
+        int noOfDaysInAMonth = new GregorianCalendar().getActualMaximum(Calendar.DAY_OF_MONTH);
+        int noOfDaysLeft = noOfDaysInAMonth - currentDate;
+        System.out.println(noOfDaysLeft + "days left in " + currentMonth + "!");
+    }
 }
