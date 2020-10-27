93337ed164b0ac423aa9f5811135f6174c316ebe
Krishan <krishan.pandey_cs19@gla.ac.in>
2020-10-27 22:24:00 +0530

    creating constructor Months

--- src/Months.java
+++ src/Months.java
@@ -1,3 +1,22 @@
 public enum Months {
+    JANUARY(31),
+    FEBRUARY(29),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+    private final int NO_OF_DAYS;
+
+    Months(int NO_OF_DAYS){
+        this.NO_OF_DAYS = NO_OF_DAYS;
+    }
+
 
 }
